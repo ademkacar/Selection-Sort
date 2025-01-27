23ddb851451dcab455c40f5da0b8717de65aa59f
Adem Kaçar <166117926+ademkacar@users.noreply.github.com>
2025-01-27 14:32:37 +0300

    Add files via upload

---
+++ selectionsort/selectionsort/selectionsort.cpp
@@ -0,0 +1,43 @@
+﻿// selectionsort.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int dizi[] = { 5,3,10, 9, 7, 1 };
+
+    int enkucuk,temp;
+
+    for (int i = 0; i < 6; i++) {
+
+        int enkucuk = dizi[i];
+
+        for (int j = i+1; j < 6; j++) {
+
+            if (enkucuk > dizi[j]) {
+                enkucuk = dizi[j];
+                temp = dizi[i];
+                dizi[i] = dizi[j];
+                dizi[j] = temp;
+     
+              
+
+            }
+                      
+            
+        }   
+        //swap(dizi[i],enkucuk);
+    }
+
+
+    for(int a =0; a<6; a++)
+        cout << dizi[a] << endl;
+  
+
+
+}
+
